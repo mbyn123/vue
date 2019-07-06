e005e072557768e9518b8fa59cd51d33f5b49bb8
li <1832376466@qq.com>
2019-07-07 03:42:38 +0800

    20190-07-07 vue学习

---
+++ vue 动态组件/vue-评论案例_实践/vuedemo/src/components/Add.vue
@@ -0,0 +1,58 @@
+<template>
+        <div class="col-md-4">
+          <form class="form-horizontal">
+            <div class="form-group">
+              <label for="name">姓名</label>
+              <input type="text" id="name" class="form-control" placeholder="用户名" v-model="name">
+            </div>
+            <div class="form-group">
+              <label>评论内容</label>
+                <textarea class="form-control" rows="6" placeholder="评论内容" v-model="content"></textarea>
+            </div>
+          <div class="form-group pull-right">
+            <button type="button" class="btn btn-primary" @click="add">提交</button>
+          </div>
+          </form>
+        </div>
+</template>
+
+<script>
+export default{
+  props:{
+    addComment:{
+      type:Function,
+      required:true
+    },
+    
+  },
+  data(){
+    return{
+      name:'',
+    content:''
+    }
+    
+  },
+ methods:{
+   add(){
+    const name=this.name.trim()
+    const content=this.content.trim()
+    if(!name || !content){
+      alert("输入内容不能为空")
+      return
+    }
+    const comment={
+      name,
+      content
+    }
+    this.addComment(comment)
+
+    this.content=''
+
+   }
+ }
+}
+</script>
+
+<style>
+
+</style>
---
+++ vue 动态组件/vue_hello world/VueDemo/src/components/HelloWorld.vue
@@ -0,0 +1,28 @@
+<template>
+<div>
+  <p class="msg">{{msg}}</p>
+</div>
+</template>
+
+
+<script>
+export default{//配置对象(vue一致)
+    data(){//组件必须写函数
+      return{
+          msg:'hello world vue-cli 2019-06-15'
+      }
+    }
+}
+</script>
+
+
+<style>
+  .msg{
+      font-size:20px;
+      color:red;
+  }
+</style>
+
+
+
+
